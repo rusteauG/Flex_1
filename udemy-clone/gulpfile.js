a79a631bd32b10077f29f2b45504ce499a3944be
RusteauN <rrusteau#gmail.com>
2024-08-01 18:36:08 +0400

    nested rules - cards

--- udemy-clone/gulpfile.js
+++ udemy-clone/gulpfile.js
@@ -1,15 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
-// compile
 function buildStyles() {
-	return src("*.scss").pipe(sass()).pipe(dest("css"));
+	return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
-// Watch
 function watchTask() {
-	watch(["*.scss"], buildStyles);
+	watch(["shinobi/**/*.scss"], buildStyles);
 }
 
-// run
 exports.default = series(buildStyles, watchTask);
